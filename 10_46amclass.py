36af7b8378ff064456c4e72bff5db99340f6639c
Rojina Dhakal <128691590+Rojina0803@users.noreply.github.com>
2024-05-31 11:01:40 +0545

    10_46amclass.py

--- 10_46amclass.py
+++ 10_46amclass.py
@@ -1,65 +1,65 @@
-# #Major concept of python required in Data Science
-# numbers=[1,2,3,4,5]
-# # squared_numbers=[]
-# # for numbers in numbers:
-# #     numbers= numbers*numbers
-# #     squared_numbers.append(numbers)
-# #     print(squared_numbers)
+#Major concept of python required in Data Science
+numbers=[1,2,3,4,5]
+# squared_numbers=[]
+# for numbers in numbers:
+#     numbers= numbers*numbers
+#     squared_numbers.append(numbers)
+#     print(squared_numbers)
 
-# #List Comprehension
-# num_sqrs=[numbers**2 for numbers in numbers]
-# print(num_sqrs)
-
-# # integer=[0,1,3,4,5]
-# # divided=[integer/2 for integer in integer]
-# # print(divided)
+#List Comprehension
+num_sqrs=[numbers**2 for numbers in numbers]
+print(num_sqrs)
 
 # integer=[0,1,3,4,5]
-# try:
-#     divided=[integer/0 for integer in integer]
-#     print(divided)
-# except ZeroDivisionError:
-#     print("Cannot divide by zero.")
+# divided=[integer/2 for integer in integer]
+# print(divided)
 
-# #Iterators
-# fruits=["apple","banana","cherry"]
-# for f in fruits:
-#     print(f)
-# # memory efficient,less time consuming,doesnt occupy large memory space, can acess value one at a time
-# fruits_it= iter(fruits)
-# print(next(fruits_it))
+integer=[0,1,3,4,5]
+try:
+    divided=[integer/0 for integer in integer]
+    print(divided)
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
 
-#CUSTOM ITERATORS
+#Iterators
+fruits=["apple","banana","cherry"]
+for f in fruits:
+    print(f)
+# memory efficient,less time consuming,doesnt occupy large memory space, can acess value one at a time
+fruits_it= iter(fruits)
+print(next(fruits_it))
 
-# #GENERATORS
-# #yield works as iterator
-# def squared_generator(*number):
-#     for num in number:
-#         yield num**2
-# squares=squared_generator(1,2,3,4)
-# print(next(squares))
+CUSTOM ITERATORS
 
-# #Decorator
-# #  function that takes function as input
-# def multiply(a,b):
-#     return a*b
-# result= multiply(2,4)
-# print(result)
+#GENERATORS
+#yield works as iterator
+def squared_generator(*number):
+    for num in number:
+        yield num**2
+squares=squared_generator(1,2,3,4)
+print(next(squares))
 
-# def log_function(func):
-#     def wrapper(a,b):
-#         result= func(a,b)
-#         print(f'Parameter are {a} and {b}')
-#         return result
-#     return wrapper
+#Decorator
+#  function that takes function as input
+def multiply(a,b):
+    return a*b
+result= multiply(2,4)
+print(result)
 
-# @log_function
-# def multiply(a,b):
-#     return a*b
-# result= multiply(2,4)
-# print(result)
+def log_function(func):
+    def wrapper(a,b):
+        result= func(a,b)
+        print(f'Parameter are {a} and {b}')
+        return result
+    return wrapper
 
-# #args,Kargs
+@log_function
+def multiply(a,b):
+    return a*b
+result= multiply(2,4)
+print(result)
+
+#args,Kargs
 
 
 
